a03129552ae0e594f5aa845654838e08f6b70674
somwrita <somwrita.debnath@appsbee.com>
2020-10-15 13:21:26 +0530

    profile page

--- src/components/web/Profile/detail.js
+++ src/components/web/Profile/detail.js
@@ -42,13 +42,13 @@ class ProfileDetail extends Component {
                 <h2 className="mid-heading">{this.state.details.first_name} {this.state.details.last_name}</h2>
                 <div className="content-part-wrapper profile-content-part-wrapper">
                     <div className="user-address custom-profile2-address">
-                        <p>Keller Williams</p>
-                        <p>Keller Williams Westfield</p>
-                        <p>841 North 900 West</p>
-                        <p>Orem, UT 84057</p>
-                        <p><a href="">801-555-3300</a></p>
-                        <p><a href="">email@bestrealtor.com</a></p>
-                        <p>UT Lic. #1234567</p>
+                        <p>{this.state.details.brokerage_name}</p>
+                        <p>{this.state.details.brokerage_office_name}</p>
+                        <p>{this.state.details.street_name}</p>
+                        <p>{this.state.details.city}, {this.state.details.state_code} {this.state.details.zipcode}</p>
+                        <p><a href="">{this.state.details.phone}</a></p>
+                        <p><a href="">{this.state.details.email}</a></p>
+                        <p>{this.state.details.state_code} {this.state.details.license_no}</p>
                     </div>
                 </div>
         
@@ -60,96 +60,77 @@ class ProfileDetail extends Component {
                         <a href=""><img src="assets/img/color-plus.png" /> New Invitation</a>
                     </div>
                     <ul className="profile2-list">
-            <li>
-                <div className="row d-flex align-items-center">
-                    <div className="col-lg-10">
-                    <div className="item user-block  d-flex align-items-center">
-                
-                <div className="user-block-picture">
-                    <div className="user-block-status">
-                        <div className="small-profile-alpha">B</div>
-                    </div>
-                </div>
-                <div className="profile2-list-txt">Ashley Jensen & Assoc.</div>
-                
-                </div>
-                    </div>
-                    <div className="col-lg-2 text-right">
-                    <div className="profile2-list-txt-del"><a href=""><em className="fa-2x mr-2 far fa-trash-alt"></em></a></div>
-                    </div>
+                        {
+                           (this.state.joinedGrouplist).map((joined,index) => {
+                                let letterImage = joined.group_name.charAt(0);
+                                return (
+                                    <li key={`joined_${index}`}>
+                                        <div className="row d-flex align-items-center">
+                                            <div className="col-lg-10">
+                                            <div className="item user-block  d-flex align-items-center">
+                                        
+                                        <div className="user-block-picture">
+                                            <div className="user-block-status">
+                                                
+                                                {   (joined.group_photo != '') ?
+                                                        <img className="rounded-circle" src={joined.group_photo} alt="Avatar" style={{width : '60px', height : "60px"}} />
+                                                        : <div className="small-profile-alpha">{letterImage}</div>
+                                                }
+                                                
+                                            </div>
+                                        </div>
+                                        <div className="profile2-list-txt">{joined.group_name}</div>
+                                        
+                                        </div>
+                                            </div>
+                                            <div className="col-lg-2 text-right">
+                                            <div className="profile2-list-txt-del"><a href=""><em className="fa-2x mr-2 far fa-trash-alt"></em></a></div>
+                                            </div>
 
-</div>
-                </li>
-            <li>
-                <div className="row d-flex align-items-center">
-                    <div className="col-lg-10">
-                    <div className="item user-block  d-flex align-items-center">
-                
-                <div className="user-block-picture">
-                    <div className="user-block-status">
-                        <img className="rounded-circle" src="img/user/02.jpg" alt="Avatar" width="60" height="60" />
-                        
-                    </div>
-                </div>
-                <div className="profile2-list-txt">Ashley Jensen & Assoc.</div>
-                
-                </div>
-                    </div>
-                    <div className="col-lg-2 text-right">
-                    <div className="profile2-list-txt-del"><a href=""><em className="fa-2x mr-2 far fa-trash-alt"></em></a></div>
-                    </div>
-
-</div>
-                </li>
-            </ul>
+                                        </div>
+                                    </li>
+                    
+                                )
+                            })
+                        }
+                    </ul>
                 </div>
             <div className="content-part-wrapper">
                 <h2 className="mid-heading-other">My Group</h2>
                 <div className="add-part">
-                    <a href=""><img src="assets/img/white-plus.png" /> New Invitation</a>
+                    <a href=""><img src="assets/img/white-plus.png" /> Create New Group</a>
                 </div>
                     <ul className="profile2-list">
-                        <li>
-                            <div className="row d-flex align-items-center">
-                                <div className="col-lg-10">
-                                    <div className="item user-block  d-flex align-items-center">
-                                
-                                        <div className="user-block-picture">
-                                            <div className="user-block-status">
-                                                <img className="rounded-circle" src="assets/img/user/02.jpg" alt="Avatar" width="60" height="60" />
-                                                
+                        {
+                            (this.state.myGrouplist).map((my,index) => {
+                                let letterImage = my.group_name.charAt(0);
+                                return (
+                                        <li key={`joined_${index}`}>
+                                        <div className="row d-flex align-items-center">
+                                            <div className="col-lg-10">
+                                            <div className="item user-block  d-flex align-items-center">
+                                        
+                                                <div className="user-block-picture">
+                                                    <div className="user-block-status">
+                                                        {   (my.group_photo != '') ?
+                                                            <img className="rounded-circle" src={my.group_photo} alt="Avatar" style={{width : '60px', height : "60px"}} />
+                                                            : <div className="small-profile-alpha">{letterImage}</div>
+                                                        }
+                                                    </div>
+                                                </div>
+                                                <div className="profile2-list-txt">{my.group_name} </div>
+                                        
                                             </div>
-                                        </div>
-                                        <div className="profile2-list-txt">Ashley Jensen & Assoc.</div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-2 text-right">
-                                    <div className="profile2-list-txt-del"><a href=""><em className="fa-2x mr-2 far fa-trash-alt"></em></a></div>
-                                </div>
+                                            </div>
+                                            <div className="col-lg-2 text-right">
+                                            <div className="profile2-list-txt-del"><a href=""><em className="fa-2x mr-2 far fa-trash-alt"></em></a></div>
+                                            </div>
 
-                            </div>
-                        </li>
-                        <li>
-                            <div className="row d-flex align-items-center">
-                                <div className="col-lg-10">
-                                <div className="item user-block  d-flex align-items-center">
-                            
-                            <div className="user-block-picture">
-                                <div className="user-block-status">
-                                    <img className="rounded-circle" src="assets/img/user/02.jpg" alt="Avatar" width="60" height="60" />
-                                    
-                                </div>
-                            </div>
-                            <div className="profile2-list-txt">Ashley Jensen & Assoc.</div>
-                            
-                            </div>
-                                </div>
-                                <div className="col-lg-2 text-right">
-                                <div className="profile2-list-txt-del"><a href=""><em className="fa-2x mr-2 far fa-trash-alt"></em></a></div>
-                                </div>
-
-            </div>
-                            </li>
+                                            </div>
+                                        </li>
+                                )
+                            })
+                        }
                     </ul>
         
         
@@ -161,17 +142,14 @@ class ProfileDetail extends Component {
 }
 
 const mapStateToProps = state => {
-    console.log(state);
-	return {
+    return {
       profiledetail  : state.brokerdetail.profiledetail,
-	 
 	}
   }
   
   const mapDispatchToProps = dispatch => {
 	return {
 		getprofileDetails : bindActionCreators(getprofileDetails , dispatch),
-	  
 	}
   }
 
--- src/components/web/Profile/index.js
+++ src/components/web/Profile/index.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { withRouter } from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
+
 
 import HeaderUser from '../HeaderUser';
 import Menu from '../Menu';
@@ -14,11 +16,11 @@ class Profile extends Component {
   constructor(props) {
     super(props);
    // setPublicIP();
-	this.state = {
-		notification : [],
-		profilesec   : {},
-		listing	     : []
-	}
+    this.state = {
+      profilepicture : '',
+      name           : '',
+      rating         : 3.5
+    }
 
   }
  
@@ -28,17 +30,18 @@ class Profile extends Component {
 	}
 
 	UNSAFE_componentWillReceiveProps(nextProps,prevProps,prevState){  
-		this.setState({
-			notification : nextProps.dasboarddetail.list,
-			profilesec	 : nextProps.dasboarddetail.my_profile_details,
-			listing		 : nextProps.dasboarddetail.my_list,
+    this.setState({
+      profilepicture : nextProps.profiledetail.profile_photo,
+      name           : nextProps.profiledetail.first_name,
+      //rating         : Number(nextProps.profiledetail.rating)
 		})
 	}
   
  
 
-   render() {
-
+  render() {
+    let letterImage = this.state.name.charAt(0);
+    console.log(this.state.rating);
     return (
 		<div className="wrapper">
 			<HeaderUser />
@@ -56,13 +59,20 @@ class Profile extends Component {
                                         <div className="item user-block user-part no-pad">
                                             
                                             <div className="user-block-picture custom-user-block-picture2">
-                                                <div className="user-block-status d-flex align-items-center">
-                                                        <div className="alpha">A</div>
+                                                <div className={(this.state.profilepicture != '') ? 'user-block-status' : 'user-block-status d-flex align-items-center'}>
+                                                        {
+                                                          (this.state.profilepicture != '') ?
+                                                            <img className="img-thumbnail rounded-circle" src={this.state.profilepicture} alt="Avatar" />
+                                                            :  <div className="alpha">{letterImage}</div>
+                                                        }
+                                                        
 
-                                                        <div className="pic-edit"><label htmlFor="up"><img className="" src="assets/img/edit-image.png" alt="Avatar" /></label></div>
+                                                        <div className="pic-edit">
+                                                          <label htmlFor="up">
+                                                            <img className="" src="assets/img/edit-image.png" alt="Avatar" />
+                                                          </label>
+                                                        </div>
                                                         <input type="file" id="up"style={{display : "none"}} />
-                                                
-                                                    
                                                 </div>
 
                                             </div>
@@ -71,7 +81,21 @@ class Profile extends Component {
                
                                     <div className="content-part-wrapper text-center">
                                     <h2 className="mid-heading">RATING</h2>
-                                        <p><img className="" src="assets/img/star-icon.png" /></p>
+                                        <div style={{marginLeft: '50px'}}>            
+                                            <ReactStars
+                                              value={this.state.rating}
+                                              size={20}
+                                              count= {5}
+                                              color= "#00FFFF"
+                                              activeColor= "#00FFFF"
+                                              edit = {false}
+                                              isHalf= {true} 
+                                              a11y = {true}
+                                              emptyIcon = {<i className="far fa-star" />}
+                                              halfIcon= {<i className="fa fa-star-half-alt" />}
+                                              filledIcon= {<i className="fa fa-star" />}
+                                            />
+                                        </div>   
                                     </div>
                                 </div>
 
@@ -87,17 +111,15 @@ class Profile extends Component {
 
 const mapStateToProps = state => {
 	return {
-	  dasboarddetail  : state.dashboarddetail.dashboard,
-	 
+    profiledetail  : state.brokerdetail.profiledetail,
 	}
-  }
+}
   
-  const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = dispatch => {
 	return {
 		dashboardRequest : bindActionCreators(dashboardRequest , dispatch),
-	  
 	}
-  }
+}
 
 export default connect(
   mapStateToProps,
