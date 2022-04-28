79bc39f0c0a499c35517e9fb2ca03d8be0560345
Haninder Singh <haninder.singh@appsbee.com>
2022-04-28 19:59:11 +0530

    plaid

--- src/components/web/ConnectAccount/index.js
+++ src/components/web/ConnectAccount/index.js
@@ -11,14 +11,14 @@ import { createAccountRequest } from "../../../actions/web/authAction";
 
 import { TrendingUpTwoTone } from '@material-ui/icons';
 import {usePlaidLink, PlaidLinkOptions, PlaidLinkOnSuccess,} from 'react-plaid-link';
-
+import {PlaidLink} from 'react-plaid-link'
 class ConnectAccount extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             open         : true,
-           
+            linkToken: "",
         }
         this.onOpenModal  = this.onOpenModal.bind(this);
         this.onCloseModal = this.onCloseModal.bind(this);
@@ -41,7 +41,8 @@ class ConnectAccount extends Component {
     }
 
     componentDidMount(){
-      
+      this.setState({linkToken: localStorage.getItem('link_token')});
+     
       let userDetails = JSON.parse(localStorage.getItem('userDetails'));
       if((userDetails.payment_onboard_acc_id != null) || (userDetails.payment_onboard_acc_id != '')){
         let elmButton = document.querySelector("#submit");
@@ -99,10 +100,74 @@ class ConnectAccount extends Component {
 
     }
 
-    
+    // handleOnSuccess(token, metadata) {
+    //   // send token to client server
+    // }
+    handleOnExit() {
+      // handle the case when your user exits Link
+    }
+ 
+    handleOnSuccess(public_token, metadata) {
+      console.log(public_token);
+     
+     fetch(`${apiURLPrefix}/plaid/exchange_public_token`, {
+        method: "POST",
+        headers: {
+          Authorization     : 'Bearer ' + localStorage.getItem('jwtToken'),
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          public_token : public_token,
+          account_id : metadata.account_id
+        })
+      })
+        .then(response => response.json())
+        .then(data => { 
+          console.log('mydata', data);
+          if(data.status_code === 200)
+          {
+
+            fetch(`${apiURLPrefix}/dwolla/update_dwolla_customer`, {
+              method: "POST",
+              headers: {
+                Authorization     : 'Bearer ' + localStorage.getItem('jwtToken'),
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify(
+              {
+                user_type : data.response.user_type,
+                first_name:data.response.first_name,
+                last_name:data.response.last_name,
+                email_id:data.response.email,
+                address1 : "Laketown",
+                address2  : "",
+                state_id : 2,
+                city	: "test",
+                state_code : "MT",
+                postal_code : "12345",
+                dateOfBirth : "1992-08-23",
+                ssn : "1234"
+              })
+            })
+              .then(response => response.json())
+              .then(data => { 
+               console.log('dwalalaupdate---',data);
+              });
+
+          }
+        });
+
+      // send token to client server
+    }
+
+    updateDwollaCustomer(data){
+
+    } 
     render() {
+      const {linkToken} = this.state
+     // const {linkToken} = localStorage.getItem('link_token');
         return (
-            
+         
             <Modal open={this.state.open} onClose={this.onCloseModal} showCloseIcon={false} center id="payment-modal">
                 
             
@@ -113,14 +178,32 @@ class ConnectAccount extends Component {
                     </div>
                   
                     <div className="ver-frm-wrapper">
-                        <button id="submit">CONNECT TO STRIPE</button>
+                    {localStorage.getItem('link_token').toString()}
+                  
+                    
+                    
+                       {
+                       localStorage.getItem('link_token').toString()!== 'undefined' ? 
+       <PlaidLink 
+       token={localStorage.getItem('link_token').toString()} 
+       env="sandbox" 
+       onSuccess={this.handleOnSuccess}
+       onExit={this.handleOnExit}>
+         Connect To Plaid
+         </PlaidLink> 
+         : null
+        } 
+      
+      
+                   
+                        {/* <button id="submit">CONNECT TO STRIPE</button>
                         {
                           (sessionStorage.getItem('connectfromlisting') == '1')  ? 
                           <p className="skip"><a href={void(0)} id="submit2" onClick={() => this.onSkip('BACK')}>BACK</a></p> :
                           <p className="skip"><a href={void(0)} id="submit2" onClick={() => this.onSkip('SKIP')}>SKIP</a> FOR NOW</p> 
 
                         }
-                        
+                         */}
                                 
                     </div>
                   </div>
